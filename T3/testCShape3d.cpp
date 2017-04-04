ec1ef20bc1a4ef3a8823aab6a5e75f2e9e823dfc
Lucas Pereira Luiz <lucaspereira2595@gmail.com>
2017-04-04 20:22:39 -0300

    Moving T3 files to the correct directory

--- T3/testCShape3d.cpp
+++ T3/testCShape3d.cpp
@@ -5,27 +5,24 @@
  *
  * Saida do programa:
  *
- * Volume do paralelograma: 730.17 
+ * Volume do paralelograma: 730.17
  * Volume do cone: 5.12707
  * Volume do cilindro: 20.3575
  *
  *
  * Autor: Rob McGregor
  * Data: ??/??/??
- * 
+ *
  * Ultima Alteracao: Eduardo Augusto Bezerra
  * Data da ultima alteracao: 29/05/2003
  *
  */
 
 #include "CShape3d.cpp"
-#include "CBox.cpp"
-#include "CCone.cpp"
-#include "CCylinder.cpp"
 
 int main(){
 	CShape3d* shape;
-
+	
 	// Um ponteiro para CShape3d assume a forma de um CBox
 	//
 	shape = new CBox(10.5f, 12.2f, 5.7f);
@@ -46,4 +43,3 @@ int main(){
 
 	return 0;
 }
-
