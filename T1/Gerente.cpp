b1495b3634bbb62f9d96cfacff08febe0429a105
pedrohfornari <pedrohkf14@gmail.com>
2017-04-11 20:08:05 -0300

    modulos de cadastrar e deletar implementados, falta testar

--- T1/Gerente.cpp
+++ T1/Gerente.cpp
@@ -7,29 +7,59 @@
 #include "Gerente.h"
 using namespace std;
 
-  Gerente(void)
-  {
-
-  }
-  Gerente(void);
+  Gerente(void){}
+  ~Gerente(void){}
 
   void Gerente::cadastra()
   {
     int i = 0;
-    while((alunos[i].get_matricula() != 0)&&(i<20))
+    int matricula = 0;
+    char ok = 0;
+    while (!ok){
+      cin>>matricula;
+      for(i = 0; i<20; i++)
+      {
+        if (matricula == turma(i).get.matricula())
+        {
+          cout<<"matricula já existente, por favor digite outro numero";
+          cin>>matricula;
+          i = -1;
+        }
+      }
+      i = 0;
+      while((turma(i).get_matricula() != 0)&&(i<20)
+      {
+        i++
+      }
+      if (i>=20)
+      {
+        cout<<"turma lotada, volte outro dia";
+      }
+      else
+      {
+        turma(i) = new Aluno(matricula);
+      }
+      return;
+  }
+
+  void Gerente::deleta(unsigned int matricula)
+  {
+    int i = 0;
+    while()(matricula != turma(i).get_matricula())&&(i<=20))
     {
       i++;
     }
-    if(i>=20)
+
+    if (i>=20)
     {
-      cout << "turma lotada" << endl;
+      cout<<"Matricula nao existente";
     }
     else
     {
-      cin.get
+      delete turma(i);
     }
   }
-  void Gerente::deleta(unsigned int matricula);
+
   void Gerente::consulta_um(unsigned int matricula);
   void Gerente::consulta_todos(void);
   void Gerente::altera_aluno(void);
